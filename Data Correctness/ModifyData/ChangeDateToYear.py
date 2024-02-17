b5ced1b4070e3cba383579234196d00f06295015
rianduong <pmyrd13@nottingham.ac.uk>
2024-02-17 17:38:40 +0000

    modify dtype functions

---
+++ Data Correctness/ModifyData/ChangeDateToYear.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+def change_issue_date_to_year(df: pd.DataFrame) -> pd.DataFrame:
+    newdf = df.copy()
+    newdf['issue_year'] = pd.to_datetime(newdf['issue_d']).dt.year
+    return newdf
+
+
+def change_dtype_to_datetime(ser: pd.Series) -> pd.Series:
+    newser = pd.to_datetime(ser).dt.strftime('%m-%Y')
+    return newser
